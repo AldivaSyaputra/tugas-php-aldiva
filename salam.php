0357c69195db13518df22ad10fa141385cc1f115
AldivaSyaputra <aldivasyaputra2021@gmail.com>
2025-06-29 20:57:49 +0700

    praktek

---
+++ salam.php
@@ -0,0 +1,13 @@
+<?php
+$name_presiden = $_GET['Prabowo'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Salam, Pak <?php echo $name_presiden; ?></title>
+</head>
+<body>
+    <h1><?php echo $name_presiden; ?> adalah Presiden Indonesia.</h1>
+</body>
+</html>
